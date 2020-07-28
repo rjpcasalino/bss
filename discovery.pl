ed71b5cade006eae8402fe41b4c47468bc123169
rjpcasalino <ryan@boringtranquility.io>
2020-07-28 00:34:31 -0700

    clean up

--- discovery.pl
+++ discovery.pl
@@ -6,13 +6,14 @@ use warnings;
 use Cwd;
 use File::Path qw(make_path);
 use Text::Markdown 'markdown';
+my $build;
 my $dir = getcwd;
 my $mode;
-my $file;
 
 init(@ARGV);
+
 sub init {
-	if (defined($ARGV[0]) && $ARGV[0] =~ /^--d/i) {
+	if (defined $ARGV[0] && $ARGV[0] =~ /^--d/i) {
 		$mode = "DEBUG";
 	} else {
 		$mode = "NORMAL";
@@ -23,63 +24,79 @@ sub init {
 		printf "Working in: %s\n", $dir;
 		my $time = localtime();
 		print "Local time: $time\n\n";
-		start($dir)
-	} elsif ($mode eq "NORMAL" && defined($ARGV[0]) && $ARGV[0] =~ /start/i) {
-		start($dir)
-	} else {
-		printf "Welcome!\n Enter a command. (hint: H or h gives help)\n";
-		my $answer = <STDIN>;
-		if ($answer =~ /^h/i) {
-			# TODO: create man page with troff and gzip it
-			# and store in /usr/share/man
-			open (MAN, "| man discovery");
-			close MAN;
-			return 0;
-		} elsif ($answer =~ /^ping/i) {
-			open (PING, "|ping boringtranquility.io");
-			close PING;
-		}
+		# TODO: 
+		# log format switch 
+	} elsif ($mode eq "NORMAL") {
+		printf "MODE: %s\n", $mode;
+		printf "Working in: %s\n", $dir;
+	} 
+	
+	printf "Welcome!\n Enter a command. (hint: H or h gives help)\n";
+	my $answer = <STDIN>;
+	if ($answer =~ /^h/i) {
+		# TODO: 
+		# create man page with troff and gzip it to store in /usr/share/man
+		open (MAN, "| man discovery");
+		close MAN;
+		die;
+	} elsif ($answer =~ /^s/i) {
+		print "Starting Discovery...\n";
+		start($dir);
 	}
-	print "Goodbye!";
+	print "Remember, don't give in!\n Never never never give in.\n Stay well, small fry.\n Goodbye and good luck!";
+	die;
 }
 
 sub writehtml {
 	my @file = @_;
-	open my $info, $file[0] or die "Could not open: $!";
+	open my $line, $file[0] or die "Could not open: $!";
 	$file[0] =~ s/\.md$/\.html/;
-	open my $fh, ">", "$file[0]";
-	while(my $line = <$info>)  {
-		my $html = markdown($line);
-		print {$fh} $html;
+	open my $fh, ">", $file[0] or die "err: $!";
+	while(<$line>) {
+		# TODO: 
+		# this is messy and I'm not even sure how the next if solved my problem????
+		# Yep, I'm sleepless in Seattle. Could be worse to not know why one doesn't 
+		# understand something...
+		next if $. == 1;
+		if ($_ =~ /^:[tl]:\w+::/i) {
+			if ($_ =~ /^:[tT]/) {
+				print "Title is: $_";
+			} elsif ($_ =~ /^:[lL]/) {
+				print "Layout is: $_";
+			}
+			$_ = join("", split(/:[tlTL]:/, $_));
+			$_ =~ tr/:://d;
+			# TODO: use title and layout in template...
+			print $_;
+		} else {
+			my $html = markdown($_);
+  			print {$fh} $html;
+		}
+	}
+}
+
+my @dirs;
+sub survey {
+	my $file = @_;
+	foreach $file (<*>) {
+		if (-d $file && $file ne "." && $file ne "..") {
+      			push(@dirs, $file);
+    		} elsif ($file =~ /\.md$/) {
+			print $file;
+			writehtml($file);
+		}
 	}
 }
 
 sub start {
 	opendir my $dh, $_[0] or die "Failed to open $_[0]: $!";
-	my @dirs;
 	my @files = readdir $dh;
-	foreach $file (<*>) {
-		if (-d $file && $file ne "." && $file ne "..") {
-      			push(@dirs, $file);
-    		} elsif ($file =~ /\.md$/) {
-			writehtml($file);
-		}
-	}
+	print "HELLP";
+	survey();
 	foreach $dir (@dirs) {
 		chdir($dir);
 		my @files = readdir $dh;
-		foreach $file (<*>) {
-			if (-d $file && $file ne "." && $file ne "..") {
-				push(@dirs, $file);
-			} elsif ($file =~ /\.md$/) {
-				writehtml($file);
-			}
-		}
+		print $dir;
+		survey();
 	}
-	#make_path "./_site/$dir" or die "err mkpath $dir: $!";
 }
-
-format STDOUT_TOP = 
-Discovery v0.01
-Copyright Ryan Casalino, 2020
-.
