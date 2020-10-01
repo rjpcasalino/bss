0d2a47a7b89443611a162be05ccaca76a23a84f4
Ryan <jobs@boringtranquolity.io>
2020-10-01 09:34:13 -0700

    adds basic web server

---
+++ lib/Web.pm
@@ -0,0 +1,109 @@
+package Web;
+# Core Web server rounties from
+# Chapter 15 of "Network Programming with Perl"
+# copyright Linclon D Stein 
+
+use v5.10;
+
+use vars '@ISA', '@EXPORT';
+require Exporter;
+
+@ISA = 'Exporter';
+@EXPORT = qw(handle_connection docroot);
+
+my $DOCUMENT_ROOT = '/home/rjpc/programs/bss/example/_site';
+my $CRLF = "\015\012";
+
+sub handle_connection {
+	my $c = shift; #socket
+	my ($fh, $type, $length, $url, $method);
+	local $/ = "$CRLF$CRLF"; # set end of line character
+	my $request = <$c>; # read request header
+
+	return invalid_request($c)
+	 unless ($method, $url) = $request =~ m!^(GET|HEAD) (/.*) HTTP/1\.[01]!;
+	return not_found($c) unless ($fh, $type, $length) = lookup_file($url);
+	return redirect($c, "$url/") if $type eq 'directory';
+
+	# print the header
+	print $c "HTTP/1.0 200 OK$CRLF";
+	print $c "Content-length: $length$CRLF";
+	print $c "Content-type: $type$CRLF";
+	print $c $CRLF;
+
+	return unless $method eq 'GET';
+
+	# print the content
+	my $buffer;
+	while ( read($fh, $buffer, 1024) ) {
+		print $c $buffer;
+	}
+	close $fh;
+}
+
+sub lookup_file {
+	my $url = shift;
+	my $path = $DOCUMENT_ROOT . $url; # turn into path
+	$path =~ s/\?.*$//; # ger rid of query
+	$path =~ s/\#.*$//; # get rid of fragment
+	$path .= 'index.html' if $url =~ m!/$!; # get index.html if path ends in /
+	return if $path =~ m!/\.\\./!; # don't allow relative paths (..)
+	return (undef, 'directory', undef) if -d $path; # oops! a directory
+	my $type = 'text/plain'; # default MIME type
+	$type = 'text/html' if $path =~ /\.html?$/i; # HTML file?
+	$type = 'text/gif' if $path =~ /\.gif?$/i; # gif file?
+	$type = 'text/jpeg' if $path =~ /\.jpe?g$/i; # jpg file?
+	return unless my $length = (stat(_))[7]; # file size
+	return unless my $fh = IO::File->new($path, "<"); # try to open file
+}
+
+sub redirect {
+	my ($c, $url) = @_;
+	my $hots = $c->sockhost;
+	my $port = $c->sockport;
+	my $moved_to = "http://$host:$port$url";
+	print $c "HTTP/1.0 301 Moved permanently$CRLF";
+	print $c "Location: $moved_to$CRLF";
+	print $c "Content-type: text/html$CRLF$CRLF";
+	print $c <<END;
+<HTML>
+<HEAD><TITLE>301 Moved</TITLE></HEAD>
+<BODY><H1>MOVED</H1>
+<p> The requested document has moved <a href="$moved_to">here</a>.<.p>
+</BODY>
+</HTML>
+END
+}
+
+sub invalid_request {
+	my $c = shift;
+	print $c "HTTP/1.0 400 Bad Request$CRLF";
+	print $c "Content-type: text/html$CRLF$CRLF";
+	print $c <<END;
+<HTML>
+<HEAD><TITLE>400 Bad Request</TITLE></HEAD>
+<BODY><H1>Bad Request</H1>
+</BODY>
+</HTML>
+END
+}
+
+sub not_found {
+	my $c = shift;
+	print $c "HTTP/1.0 404 Not Found$CRLF";
+	print $c "Content-type: text/html$CRLF$CRLF";
+	print $c <<END;
+<HTML>
+<HEAD><TITLE>404 Not Found</TITLE></HEAD>
+<BODY><H1>404 Not Found</H1>
+</BODY>
+</HTML>
+END
+}
+
+sub docroot {
+	$DOCUMENT_ROOT = shift if @_;
+	return $DOCUMENT_ROOT;
+}
+
+1; # perl programs end this way :-) 
