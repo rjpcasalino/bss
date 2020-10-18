5527362a180ab4358ed9c42ed9bda44b819ffe24
Ryan <jobs@boringtranquolity.io>
2020-10-17 17:00:33 -0700

    rans perltidy

--- bss.pl
+++ bss.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 
-# bss - boring static site generator 
+# bss - boring static site generator
 # Copyright (C) 2020  Ryan Joseph Patrick Casalino
 #
 
@@ -39,56 +39,61 @@ use Web;
 my $manifest = "manifest.ini";
 say "No manifest.ini found!" and exit unless -e $manifest;
 
-$manifest = Config::IniFiles->new(-file => "manifest.ini");
+$manifest = Config::IniFiles->new( -file => "manifest.ini" );
 my %config = (
-	TT_CONFIG => \%tt_config,
-	TT_DIR => realpath ($manifest->val("build", "templates_dir") // "templates"),
-	SRC => $manifest->val("build", "src") // "src", # TODO: disallow back/forward slashes 
-	DEST => $manifest->val("build", "dest") // "_site",
-	ENCODING => $manifest->val("build", "encoding") // "UTF-8",
-	COLLECTIONS => $manifest->val("build", "collections") // undef,
-	WATCH => $manifest->val("build", "watch") // "false",
-	EXCLUDE => $manifest->val("build", "exclude") // "*.md, templates",
-	PORT => $manifest->val("server", "port") // "9000", 
-	HOST => $manifest->val("server", "host") // "localhost"
+    TT_CONFIG => \%tt_config,
+    TT_DIR =>
+      realpath( $manifest->val( "build", "templates_dir" ) // "templates" ),
+    SRC => $manifest->val( "build", "src" )
+      // "src",    # TODO: disallow back/forward slashes
+    DEST        => $manifest->val( "build", "dest" )        // "_site",
+    ENCODING    => $manifest->val( "build", "encoding" )    // "UTF-8",
+    COLLECTIONS => $manifest->val( "build", "collections" ) // undef,
+    WATCH       => $manifest->val( "build", "watch" )       // "false",
+    EXCLUDE => $manifest->val( "build",  "exclude" ) // "*.md, templates",
+    PORT    => $manifest->val( "server", "port" )    // "9000",
+    HOST    => $manifest->val( "server", "host" )    // "localhost"
 );
 
 # TEMPLATE TOOLKIT #
 my $tt_config = {
-	INCLUDE_PATH => undef,
-	INTERPOLATE  => 0,
-	POST_CHOMP   => 1,
-	EVAL_PERL    => 1,
-	RELATIVE => 1,		    
-	ENCODING => undef
+    INCLUDE_PATH => undef,
+    INTERPOLATE  => 0,
+    POST_CHOMP   => 1,
+    EVAL_PERL    => 1,
+    RELATIVE     => 1,
+    ENCODING     => undef
 };
 
-mkdir($config{DEST}) unless -e $config{DEST};
+mkdir( $config{DEST} ) unless -e $config{DEST};
+
 # set template toolkit options
 $config{TT_CONFIG}->{INCLUDE_PATH} = $config{TT_DIR};
-$config{TT_CONFIG}->{ENCODING} = $config{ENCODING};
+$config{TT_CONFIG}->{ENCODING}     = $config{ENCODING};
 
-# TODO: build is a command not an option. server as well? 
+# TODO: build is a command not an option. server as well?
 # also don't forget about 'watch'
-my %opts = (build => '', server => '', verbose => '', help => '');
+my %opts = ( build => '', server => '', verbose => '', help => '' );
 
-GetOptions(\%opts, qw(
-   build
-   verbose
-   help
-   server
-   watch
-));
+GetOptions(
+    \%opts, qw(
+      build
+      verbose
+      help
+      server
+      watch
+      )
+);
 
 do_build(%config) if $opts{build};
-server() if $opts{server};
+server()          if $opts{server};
 
 pod2usage(1) if $opts{help};
 
 say "--manifest--" if $opts{verbose};
-foreach $key (sort keys %config) {
-	$value = $config{$key};
-	say "$key: $value" if $opts{verbose};
+foreach $key ( sort keys %config ) {
+    $value = $config{$key};
+    say "$key: $value" if $opts{verbose};
 }
 
 $greetings = "Hello!\t Bonjour!\t Welcome!\t ひ!\t\n";
@@ -101,107 +106,129 @@ say "
 	Server -
 	 PORT:$config{PORT}
 	 "
-if $opts{verbose};
+  if $opts{verbose};
 
 sub do_build {
-	system "rm", "-rf", $config{DEST};
-	@collections = split /,/, $config{COLLECTIONS};
-	my %collections = ();
-	for my $dir (@collections) {
-		# this pushes an empty list into the hash...
-		push( @{ $collections { $dir } }, ());
-		find(sub { next if $_ eq "." or $_ eq ".."; push @{$collections{$dir}}, $_}, File::Spec->catfile($config{SRC},$dir));
-		$config{COLLECTIONS} = \%collections;
-	}
-	find(\&build, $config{SRC});
-	open my $exclude_fh, ">", "exclude.txt";
-	@excludes = split /,/, $config{EXCLUDE};
-	for $line (@excludes) {
-		say $exclude_fh "$line";
-	}
-	system "rsync", "-avmh", "--exclude-from=exclude.txt", $config{SRC}, $config{DEST};
-	# house cleaning
-	unlink("exclude.txt");
-	find(sub {if ($_=~ /.html$/) { unlink($_)}}, $config{SRC});
-	# thanks for stopping by!
-	say "Site created in $config{DEST}!";
-	exit;
+    system "rm", "-rf", $config{DEST};
+    @collections = split /,/, $config{COLLECTIONS};
+    my %collections = ();
+    for my $dir (@collections) {
+
+        # this pushes an empty list into the hash...
+        push( @{ $collections{$dir} }, () );
+        find(
+            sub {
+                next if $_ eq "." or $_ eq "..";
+                push @{ $collections{$dir} }, $_;
+            },
+            File::Spec->catfile( $config{SRC}, $dir )
+        );
+        $config{COLLECTIONS} = \%collections;
+    }
+    find( \&build, $config{SRC} );
+    open my $exclude_fh, ">", "exclude.txt";
+    @excludes = split /,/, $config{EXCLUDE};
+    for $line (@excludes) {
+        say $exclude_fh "$line";
+    }
+    system "rsync", "-avmh", "--exclude-from=exclude.txt", $config{SRC},
+      $config{DEST};
+
+    # house cleaning
+    unlink("exclude.txt");
+    find(
+        sub {
+            if ( $_ =~ /.html$/ ) { unlink($_) }
+        },
+        $config{SRC}
+    );
+
+    # thanks for stopping by!
+    say "Site created in $config{DEST}!";
+    exit;
 }
 
 sub server {
-	my $port = $config{PORT};
-	my $socket = IO::Socket::INET->new( LocalPort => $port,
-				   	    Listen => SOMAXCONN,
-			    	    	    Reuse => 1 )
-			    	    	    or die "Can't create listen socket: $!";
-	say "Started local dev server on $port!";
-	while (my $c = $socket->accept) {
-		handle_connection($c);
-		close $c;
-	}
-	close $socket;
+    my $port   = $config{PORT};
+    my $socket = IO::Socket::INET->new(
+        LocalPort => $port,
+        Listen    => SOMAXCONN,
+        Reuse     => 1
+    ) or die "Can't create listen socket: $!";
+    say "Started local dev server on $port!";
+    while ( my $c = $socket->accept ) {
+        handle_connection($c);
+        close $c;
+    }
+    close $socket;
 }
 
 sub handleYAML {
-	use YAML;
-	my $yaml;
-	open $MD, $_;
-	undef $/;
-	my $data = <$MD>;
-	if ($data =~ /---(.+)---/s) {
-		$yaml = Load($1);
-	}
-	writehtml($_, $yaml);
+    use YAML;
+    my $yaml;
+    open $MD, $_;
+    undef $/;
+    my $data = <$MD>;
+    if ( $data =~ /---(.+)---/s ) {
+        $yaml = Load($1);
+    }
+    writehtml( $_, $yaml );
 }
 
 sub writehtml {
-	my ($html, $yaml) = @_;
-	$html =~ s/\.md$/\.html/;
-	
-	my $template = Template->new($config{TT_CONFIG});
-	my $layout; 
-	my @body;
-	
-	open $MD, $_;
-	while(<$MD>) {
-		# remove YAML block
-		# FIXME
-		# this just subs in blank lines, which we do not want
-		if ($_ =~ /(---(.+)---)/s) {
-			s/$1//g;
-		}
-		push(@body, markdown($_));
-	}
-	open my $HTML, ">", $html;
-	
-	my $site_modified = strftime '%c', localtime();
-	# my $page_modified;
-	
-	my $vars = {
-		title => $yaml->{title},
-		body => \@body,
-		collections => $config{COLLECTIONS},
-		site_modified => $site_modified,
-	};
+    my ( $html, $yaml ) = @_;
+    $html =~ s/\.md$/\.html/;
 
-	$template->process("$yaml->{layout}.tmpl", $vars, $HTML)
-		or die $template->error();
-	say "$yaml->{title} processed." if $opts{verbose};
+    my $template = Template->new( $config{TT_CONFIG} );
+    my $layout;
+    my @body;
+
+    open $MD, $_;
+    while (<$MD>) {
+
+        # remove YAML block
+        # FIXME
+        # this just subs in blank lines, which we do not want
+        if ( $_ =~ /(---(.+)---)/s ) {
+            s/$1//g;
+        }
+        push( @body, markdown($_) );
+    }
+    open my $HTML, ">", $html;
+
+    my $site_modified = strftime '%c', localtime();
+
+    # my $page_modified;
+
+    my $vars = {
+        title         => $yaml->{title},
+        body          => \@body,
+        collections   => $config{COLLECTIONS},
+        site_modified => $site_modified,
+    };
+
+    $template->process( "$yaml->{layout}.tmpl", $vars, $HTML )
+      or die $template->error();
+    say "$yaml->{title} processed." if $opts{verbose};
 }
 
 sub build {
     my $filename = $_;
-    if (-d $filename) { 
-	    # ignore these dirs always:
-	    # FIXME
-	    if ($_ =~ /^$config{SRC}|^$config{TT_DIR}/) {
-		    say "Ignoring: $File::Find::name" if $opts{verbose};
-		    $File::Find::prune = 1;
-	    }
-    } elsif ($_ =~ /.md$/) {
-	    handleYAML();
-    } elsif ($_ =~ /.png|.jpg|.jpeg|.gif|.svg$/i) {
-	    # TODO
+    if ( -d $filename ) {
+
+        # ignore these dirs always:
+        # FIXME
+        if ( $_ =~ /^$config{SRC}|^$config{TT_DIR}/ ) {
+            say "Ignoring: $File::Find::name" if $opts{verbose};
+            $File::Find::prune = 1;
+        }
+    }
+    elsif ( $_ =~ /.md$/ ) {
+        handleYAML();
+    }
+    elsif ( $_ =~ /.png|.jpg|.jpeg|.gif|.svg$/i ) {
+
+        # TODO
     }
 }
 
