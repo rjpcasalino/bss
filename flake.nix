e667062781ba191422de3b93856b38ac7385abd7
rjpc <ryan@boringtranquility.io>
2021-05-21 13:07:33 -0700

    add https://github.com/numtide/flake-utils

--- flake.nix
+++ flake.nix
@@ -2,24 +2,29 @@
   description = "A flake for building bss";
 
   inputs.nixpkgs.url = github:NixOS/nixpkgs/af0a54285ed4ff131f205517aeafb94a9a5898cb;
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }: {
-    defaultPackage.x86_64-darwin =
-    with import nixpkgs { system = "x86_64-darwin"; };
-      buildPerlPackage {
-      pname = "bss";
-      version = "0.1";
-      src = self;
-      propagatedBuildInputs = [
-       perlPackages.TemplateToolkit
-       perlPackages.ModuleInstall
-       perlPackages.ConfigIniFiles
-       perlPackages.YAML
-       perlPackages.TextMarkdown
-      ];
-      buildInputs = [ 
-       rsync
-      ];
-    };
-  };
+  outputs = { self, nixpkgs, flake-utils }: 
+  flake-utils.lib.eachDefaultSystem (system:
+    with nixpkgs.legacyPackages.${system};
+    let bss = 
+        buildPerlPackage {
+        pname = "bss";
+        version = "0.1";
+        src = self;
+        propagatedBuildInputs = [
+         perlPackages.TemplateToolkit
+         perlPackages.ModuleInstall
+         perlPackages.ConfigIniFiles
+         perlPackages.YAML
+         perlPackages.TextMarkdown
+        ];
+        buildInputs = [ 
+         rsync
+        ];
+      };
+    in rec {
+      defaultPackage = bss;
+      }
+  );
 }
