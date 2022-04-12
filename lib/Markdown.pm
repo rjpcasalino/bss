9b4bbdd0ca95c5337804d3972fec9b3fe43d3f85
rjpc <ryan@boringtranquility.io>
2022-04-12 08:19:53 -0700

    gives credit to naoya

--- lib/Markdown.pm
+++ lib/Markdown.pm
@@ -1,23 +1,22 @@
-# TODO:
-# give credit to OG author
+# This is mostly Markdown.pm,v 1.3 2005/11/12 03:28:09 by naoya
+# see: https://metacpan.org/release/NAOYA/Template-Plugin-Markdown-0.02
 package Markdown;
+
 use strict;
-use base qw (Template::Plugin::Filter);
 use Text::Markdown;
- 
-our $VERSION = 0.02;
- 
+use base qw (Template::Plugin::Filter);
+
 sub init {
     my $self = shift;
     $self->{_DYNAMIC} = 1;
     $self->install_filter($self->{_ARGS}->[0] || 'markdown');
     $self;
 }
- 
+
 sub filter {
     my ($self, $text, $args, $config) = @_;
     my $m = Text::Markdown->new;
     return $m->markdown($text);
 }
- 
+
 1;
