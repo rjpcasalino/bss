3e0c275bde4e7e5331d1419ac0199854ecab8157
rjpcasalino <ryan@boringtranquility.io>
2023-12-14 21:21:06 -0800

    minor updates to flake and formatting

--- flake.nix
+++ flake.nix
@@ -5,35 +5,36 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
-  flake-utils.lib.eachDefaultSystem (system:
-    with nixpkgs.legacyPackages.${system};
-    let bss =
-        perlPackages.buildPerlPackage {
-        pname = "bss";
-        version = "0.1";
-        src = self;
-        propagatedBuildInputs = with perlPackages; [
-         DateTime
-         TemplateToolkit
-         ModuleInstall
-         ConfigIniFiles
-         YAML
-         TextMarkdown
-         FilesysNotifySimple
-        ];
-        buildInputs = [
-         makeWrapper
-       ] ++ lib.optionals stdenv.isDarwin [ shortenPerlShebang ];
-        postInstall = ''
-          mkdir -p $out/bin
-          ${if stdenv.isDarwin then "shortenPerlShebang $out/lib/perl5/site_perl/${perl.version}/bss.pl" else ""}
-          wrapProgram $out/lib/perl5/site_perl/${perl.version}/bss.pl --prefix PATH : ${lib.makeBinPath[ rsync ]}
-          ln -fs $out/lib/perl5/site_perl/${perl.version}/bss.pl $out/bin/bss
-        '';
-      };
-    in {
-      defaultPackage = bss;
+    flake-utils.lib.eachDefaultSystem (system:
+      with nixpkgs.legacyPackages.${system};
+      let
+        bss =
+          buildPerlPackage {
+            pname = "bss";
+            version = "0.1";
+            src = self;
+            propagatedBuildInputs = with perlPackages; [
+              DateTime
+              TemplateToolkit
+              ModuleInstall
+              ConfigIniFiles
+              YAML
+              TextMarkdown
+            ];
+            buildInputs = [
+              makeWrapper
+            ] ++ lib.optionals stdenv.isDarwin [ shortenPerlShebang ];
+            postInstall = ''
+              mkdir -p $out/bin
+              ${if stdenv.isDarwin then "shortenPerlShebang $out/lib/perl5/site_perl/${perl.version}/bss.pl" else ""}
+              wrapProgram $out/lib/perl5/site_perl/${perl.version}/bss.pl --prefix PATH : ${lib.makeBinPath[ rsync ]}
+              ln -fs $out/lib/perl5/site_perl/${perl.version}/bss.pl $out/bin/bss
+            '';
+          };
+      in
+      {
+        defaultPackage = bss;
       }
-  );
+    );
 }
 
